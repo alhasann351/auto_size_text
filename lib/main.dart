9acf59375f92a5a70bc547bf838f3f1c0f8ffbfd
alhasann351 <alhasann352@gmail.com>
2024-03-14 22:29:25 +0600

    AutoSizeText apply

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,6 +25,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //var myGroup = AutoSizeGroup();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,10 +40,40 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Auto Size Text',
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.blue,
+            child: const AutoSizeText(
+              //group: myGroup,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              stepGranularity: 20,
+              //minFontSize: 20,
+              //maxFontSize: 60,
+              presetFontSizes: [40, 20, 14],
+              'Flutter widget that automatically resizes text to fit perfectly within its bounds.',
+              style: TextStyle(
+                fontSize: 60,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: const Text(
+              //group: myGroup,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              'Flutter widget that automatically resizes text to fit perfectly within its bounds.',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
